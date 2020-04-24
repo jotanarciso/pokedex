d0632f42fc9e8872f13c170831b455fa7a4a8731
joao <joao.narciso@acertsis.com.br>
2020-04-24 01:00:01 -0300

    ex nihilo nihil fit

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 $(document).ready(function () {
+  /* start interface  */
   setTimeout(function () {
     $("#pokeball").fadeOut("slow", function () {
       $("#bg-loading").fadeOut("slow");
@@ -7,7 +8,9 @@ $(document).ready(function () {
 
   /* 
   **************************************************************************
-  Thanks Simon Goellner for providing the code and saving my time | https://codepen.io/simeydotme/pen/PrQKgo 
+  **************************************************************************
+  thanks simon goellner for providing the code and saving my time 
+  link: https://codepen.io/simeydotme/pen/PrQKgo 
   */
 
   var $cards = $(".card");
@@ -35,4 +38,136 @@ $(document).ready(function () {
    **************************************************************************
    **************************************************************************
    */
+
+  /* end interface  */
+
+  /* start operation */
+
+  const searchBox = document.getElementById("search-box");
+  const searchButton = document.getElementById("search-button");
+  const testButton = document.getElementById("test-button");
+
+  searchButton.addEventListener("click", function () {
+    findPokemon();
+  });
+
+  testButton.addEventListener("click", function () {
+    testPokemons();
+  });
+
+  searchBox.addEventListener("keypress", function (e) {
+    if (e.which == 13) {
+      findPokemon();
+    }
+  });
+
+  async function findPokemon(test, pokemonTest) {
+    var pokemon;
+    if (!test) {
+      pokemon = searchBox.value.toLowerCase();
+      searchBox.value = "";
+    } else {
+      pokemon = pokemonTest;
+      searchBox.value = pokemonTest;
+    }
+
+    const data = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
+    await data.json().then((response) => {
+      // console.log(response) // test
+      displayPokemon(response);
+    });
+  }
+
+  function displayPokemon(pokemon) {
+    const pokemonImg = document.getElementById("pokemon-img");
+    const pokemonName = document.getElementById("pokemon-name");
+    const pokemonId = document.getElementById("pokemon-id");
+    const pokemonXP = document.getElementById("pokemon-xp");
+    const pokemonHeight = document.getElementById("pokemon-height");
+    const pokemonWeight = document.getElementById("pokemon-weight");
+    const pokemonAbilities = document.getElementById("pokemon-abilities");
+    const pokemonForms = document.getElementById("pokemon-forms");
+    const pokemonType = document.getElementById("pokemon-type");
+
+    /* manipulating DOM and entering data */
+    pokemonImg.src = pokemon.sprites["front_default"];
+    pokemonName.innerHTML = correctData(pokemon.name);
+    pokemonId.innerHTML = "#" + pokemon.id;
+    pokemonXP.innerHTML = pokemon.base_experience;
+    pokemonHeight.innerHTML = pokemon.height;
+    pokemonWeight.innerHTML = pokemon.weight;
+
+    /* abilities */
+    let abilities = "";
+    for (var i in pokemon.abilities) {
+      // entering all abilities
+      abilities +=
+        "<p class='tag'>" +
+        correctData(pokemon.abilities[i].ability.name) +
+        "</p>";
+    }
+
+    pokemonAbilities.innerHTML = ""; // clean abilities
+    pokemonAbilities.innerHTML = abilities;
+
+    /* forms */
+    let forms = "";
+    for (var i in pokemon.forms) {
+      // entering all forms
+      forms += "<p class='tag'>" + correctData(pokemon.forms[i].name) + "</p>";
+    }
+
+    pokemonForms.innerHTML = ""; // clean forms
+    pokemonForms.innerHTML = forms;
+
+    /* types */
+    let types = "";
+    for (var i in pokemon.types) {
+      // entering all types
+      types += `<a data-toggle="tooltip" data-placement="top"
+      title="${correctData(
+        pokemon.types[i].type.name
+      )}"><img src="./img/icons/${pokemon.types[i].type.name}.png"/></a>`;
+    }
+
+    pokemonType.innerHTML = ""; // clean types
+    pokemonType.innerHTML = types;
+  }
+
+  function correctData(string) {
+    // optimize string
+    if (/[-]/.test(string)) {
+      return correctData(string.replace(/-/g, "&nbsp"));
+    } else {
+      return string
+        .toLowerCase()
+        .split("&nbsp")
+        .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+        .join(" ");
+    }
+  }
+  /* end operation  */
+
+  async function testPokemons() {
+    checkAllPokemons();
+    const testTime = 1000; // time (seconds) between test requests
+    var index = 0;
+    var time = setInterval(checkAllPokemons, testTime);
+    const qtd = 20; // amount of pokémon to test
+    async function checkAllPokemons() {
+      const pokemons = await fetch(
+        `https://pokeapi.co/api/v2/pokemon/?limit=${qtd}`
+      );
+      await pokemons.json().then((response) => {
+        findPokemon((test = true), response.results[index].name);
+        index++;
+        if (index === response.results.length) {
+          abortTimer();
+        }
+      });
+    }
+    function abortTimer() {
+      clearInterval(time);
+    }
+  }
 });
